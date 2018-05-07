6eb97a1a10f7cd06fe007b70decae1945fe3da8f
Mamot <dimas.rizky.hp@gmail.com>
2018-05-07 18:57:18 +0700

    Added webhook & event handler

--- __init__.py
+++ __init__.py
@@ -1,21 +1,59 @@
+import os
+import sys
+from argparse import ArgumentParser
+
 from flask import Flask
+from linebot import(
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import(
+    InvalidSignatureError
+)
+from linebot.models import(
+    MessageEvent, TextMessage, TextSendMessage
+)
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Ganti Masbor, Serius"
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
 
-@app.route("/test")
-def masbor():
-    return "Iki yoiso masbor"
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
 
-@app.route("/test2")
-def masbor2():
-    return "awawawa"
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
 
-@app.route("/test3")
-def masbor3():
-    return "awawawa masbor ini auto"
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
 
 if __name__ == "__main__":
-    app.run()
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + '[--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, port=options.port)
