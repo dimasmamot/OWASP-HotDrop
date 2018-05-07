0f5f68fc5492e13c800021fddbee68c47a5f7f01
Mamot <dimas.rizky.hp@gmail.com>
2018-05-07 20:45:08 +0700

    hotfix #2

--- __init__.py
+++ __init__.py
@@ -3,20 +3,21 @@ import sys
 from argparse import ArgumentParser
 
 from flask import Flask, request, abort
-from linebot import(
+from linebot import (
     LineBotApi, WebhookHandler
 )
-from linebot.exceptions import(
+from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import(
-    MessageEvent, TextMessage, TextSendMessage
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
 
-channel_secret = "59ddbf04f1c9b55f927c83f93c836ba5"
-channel_access_token = "Nu2ufpRa8DhhJ7BSxJpmEILjv69vqaYzGLxF0s00Cjg0pEwuFdARlC9awrtZgR7fjXBf3gnVS6wNaI6VoPjS4NbbMhhxyne6fpj8I3SW32LMd7tmWEgOTeT/YacRHmqzHnhEIgZPhqOp4o5HFXoldgdB04t89/1O/w1cDnyilFU="
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
@@ -27,19 +28,24 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
     return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
@@ -48,9 +54,10 @@ def message_text(event):
         TextSendMessage(text=event.message.text)
     )
 
+
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + '[--port <port>] [--help]'
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
     arg_parser.add_argument('-p', '--port', default=8000, help='port')
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
